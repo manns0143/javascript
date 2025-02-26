c302d2254a1e258e161c20bb3c85f4d4c778a099
Mann <shahmann0143@gmail.com>
2025-02-27 00:33:58 +0530

    modify datatypes file

--- basics/dataTypes.js
+++ basics/dataTypes.js
@@ -2,8 +2,8 @@ let age = 22
 let name = "ted"
 let val = true
 
-console.log(typeof age)
-console.log(typeof name)
-console.log(typeof val)
-console.log(typeof null)
-console.log(typeof undefined)
+console.log(typeof age) // number
+console.log(typeof name) // string
+console.log(typeof val) // boolean
+console.log(typeof null) // object
+console.log(typeof undefined) // undefined
